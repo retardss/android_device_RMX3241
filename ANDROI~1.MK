6b6874e7fe3568bfa747a799bc221155e4beda7d
xack21 <86214526+xack21@users.noreply.github.com>
2023-02-01 18:52:06 +0530

    Add files via upload

---
+++ ANDROI~1.MK
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_oplus_mssi_64_cn.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_oplus_mssi_64_cn-user \
+    lineage_oplus_mssi_64_cn-userdebug \
+    lineage_oplus_mssi_64_cn-eng
